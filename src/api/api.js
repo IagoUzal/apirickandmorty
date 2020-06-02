a79abcb0426df80382e07e8c79b5509c8c1c8354
iago <iagouzal@gmail.com>
2020-06-02 12:16:21 +0200

    catch

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const apiUrl = "https://rickandmortyapi.com/api";
 
 function getAll() {
-  return axios.get(`${apiUrl}/character`);
+  return axios.get(`${apiUrl}/character`).catch((error) => console.log(error));
 }
 
 export default { getAll };
