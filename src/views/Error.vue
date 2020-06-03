ab2184ceab091e477683564f1b26f6a1fe0c89b4
iago <iagouzal@gmail.com>
2020-06-03 12:37:15 +0200

    view error 404

---
+++ src/views/Error.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="error">
+    <h1>
+      Upsss...😱
+    </h1>
+    <p @click="goHome()">No hay nada aquí vuelve al home</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Error",
+  methods: {
+    goHome() {
+      this.$router.push("/");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.error p {
+  font-size: 1.2rem;
+  margin-top: 2rem;
+  cursor: pointer;
+  color: #594fff !important;
+}
+</style>
