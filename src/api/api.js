eb3fe2cca89263f8bcc2b52dd96412c1c02952f9
iago <iagouzal@gmail.com>
2020-06-03 12:36:04 +0200

    async function

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,16 @@
 const axios = require("axios");
 const apiUrl = "https://rickandmortyapi.com/api";
 
-function getAll() {
-  return axios.get(`${apiUrl}/character`).catch((error) => console.log(error));
+// function getAll() {
+//   return axios.get(`${apiUrl}/character`).catch((error) => console.log(error));
+// }
+
+async function getAll() {
+  try {
+    return await axios.get(`${apiUrl}/character`);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export default { getAll };
